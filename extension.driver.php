c0b251cd2a944f034e6d1615f39179e7410936a2
Andrew Minton <me@andrewminton.co.uk>
2016-01-25 10:31:48 +0000

    Make $child an array
    Changed as per @nitriques suggestion.

--- extension.driver.php
+++ extension.driver.php
@@ -476,7 +476,9 @@ Class extension_author_roles extends Extension
 	// the second argument
 	private static function replaceChild($parent, $child) {
 		
-		if(!is_array($child)) return false;
+		if(!is_array($child)) {
+    			$child = array($child);
+		}
 		
 		foreach($parent->getChildren() as $position => $oldChild) {
 			if($oldChild->getName() == $child->getName()) {
